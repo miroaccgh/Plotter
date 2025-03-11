12396a93a89c75d698278f449f104ddb9884e9ed
miroaccgh <wabysaby@proton.me>
2025-03-11 15:20:48 +0100

    add test outline and connection test  for  module

---
+++ tests/test_mqtt.py
@@ -0,0 +1,27 @@
+import MqttClient
+
+def test_connect():
+    """
+    This test tests that an object of the MqttClient can connect to the broker.
+    """
+    # Arrange
+    client = MqttClient
+    # Act
+    client.connect()
+    connected = client.is_connected
+    # Destroy
+    client.disconnect()
+    # Assert
+    assert connected == True
+
+def test_disconnect():
+    pass
+
+def test_create_topic():
+    pass
+
+def test_read():
+    pass
+
+def test_write():
+    pass
